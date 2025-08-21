7b230c76be17de715e5de420d5ad82dd7a77fd0f
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-21 22:42:27 +0200

    stt service

--- front-end/src/app/services/stt.service.ts
+++ front-end/src/app/services/stt.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.prod';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class SttService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  transcribe(audio: File){
+    const fd = new FormData();
+    fd.append('audio', audio);
+    return this.http.post<{text: string}>(`${environment.API_BASE}/api/stt`, fd);
+  }
 }
