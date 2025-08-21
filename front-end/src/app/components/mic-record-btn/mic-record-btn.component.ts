0e5cb66e3431133e306c27856d3327441665d480
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-21 22:32:19 +0200

    create components

---
+++ front-end/src/app/components/mic-record-btn/mic-record-btn.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mic-record-btn',
+  imports: [],
+  templateUrl: './mic-record-btn.component.html',
+  styleUrl: './mic-record-btn.component.css'
+})
+export class MicRecordBtnComponent {
+
+}
---
+++ front-end/src/app/components/tts-player/tts-player.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tts-player',
+  imports: [],
+  templateUrl: './tts-player.component.html',
+  styleUrl: './tts-player.component.css'
+})
+export class TtsPlayerComponent {
+
+}
