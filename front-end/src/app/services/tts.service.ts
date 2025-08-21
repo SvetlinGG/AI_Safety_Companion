57df7ec47458267588e5e4bd9e57c34786a67288
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-21 22:45:44 +0200

    tts service

--- front-end/src/app/services/tts.service.ts
+++ front-end/src/app/services/tts.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.prod';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class TtsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  speak(text: string, lang: string = 'en'){
+    return this.http.post(`${environment.API_BASE}/api/tts`, {text, lang}, {responseType: 'blob'});
+  }
 }
