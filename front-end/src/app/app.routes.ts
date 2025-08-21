982e89a38f88ae74ca37519930724bc237cc28a4
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-21 22:55:13 +0200

    routing

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { StepsComponent } from './pages/steps/steps.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'chat', component: ChatComponent},
+    {path: 'steps', component: StepsComponent},
+    {path: 'settings', component: SettingsComponent},
+];
