43a05551de6c498178061f163382b4d8d86091d2
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-22 00:37:36 +0200

    btn component

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApiService } from './api.service';
+import { MicRecordBtnComponent } from './components/mic-record-btn/mic-record-btn.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MicRecordBtnComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- front-end/src/app/components/mic-record-btn/mic-record-btn.component.ts
+++ front-end/src/app/components/mic-record-btn/mic-record-btn.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-mic-record-btn',
+  standalone: true,
   imports: [],
   templateUrl: './mic-record-btn.component.html',
   styleUrl: './mic-record-btn.component.css'
 })
 export class MicRecordBtnComponent {
+  @Output() recorded = new EventEmitter<Blob>();
+  private mediaRecorder!: MediaRecorder;
+  isRecording = false;
+
+  async toggleRecording(){
+    if (!this.isRecording){
+      const stream = await navigator.mediaDevices.getUserMedia({audio: true});
+      this.mediaRecorder = new MediaRecorder(stream);
+      const chunk: BlobPart[] = [];
+      this.mediaRecorder.ondataavailable = e => chunk.push(e.data);
+      this.mediaRecorder.onstop = () => this.recorded.emit(new Blob(chunk, {type: 'audio/webm'}));
+      this.mediaRecorder.start();
+      this.isRecording = true;
+    }else {
+      this.mediaRecorder.stop();
+      this.isRecording = false;
+    }
+  }
 
 }
