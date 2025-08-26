1313af89754e00ae2418e394163556dcd4e06e8f
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-26 23:11:51 +0200

    main ts

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { I18nService } from './app/services/i18n.service';
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .then((ref) => {
+    const i18n = ref.injector.get(I18nService);
+    return i18n.init();
+  })
+  .catch(err => console.error(err));
